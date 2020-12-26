cdea7add570f50659a60edab02858a35e2ef2e7f
TeuerStein <xenkai123456789@gmail.com>
2020-12-26 21:16:21 +0100

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/CppUniversityProject.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CppUniversityProject.dir/main.cpp.o"
+  "CppUniversityProject"
+  "CppUniversityProject.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CppUniversityProject.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/CppUniversityProject.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for CppUniversityProject.
+# This may be replaced when dependencies are built.
