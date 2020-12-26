f273688041e3e1652b429211d2d461c950c7652a
TeuerStein <xenkai123456789@gmail.com>
2020-12-26 23:40:22 +0100

    Was created a Warrior class with constructor and variables for him (for creating a champ with warrior game class). Was Added a types of damage for game classes

--- Models/ClassesOfHeroes.h
+++ Models/ClassesOfHeroes.h
@@ -3,19 +3,38 @@
 //
 
 #include <iostream>
+#include <stdlib.h>
+#include "TypesOfDamage.cpp"
+
+using namespace Model;
 
 #ifndef CPPUNIVERSITYPROJECT_CLASSESOFHEROES_H
 #define CPPUNIVERSITYPROJECT_CLASSESOFHEROES_H
 
+namespace Model {
+    class Warrior { // * TEST STATE OF *
+        // Class for getting a Warrior class
+        // of hero like a game champ
 
-namespace DungeonMaster {
-    class ClassesOfHeroes { // * TEST STATE OF *
+        // Init all variables what we need for
+        std::string nameOfClass;
+        int baseDamage;
+        int baseHealth;
+        int baseArmor;
+        TypesOfDamage typeOfDamage;
 
     public:
-        static int printSomeVariable(int a) {
-            std::cout << a << std::endl;
+        // Getting all variables into constructor
+        // for creating a warrior with unique stats
 
-            return 0;
+        void ConstructorForWarriorModel() {
+            nameOfClass = "Warrior";
+            baseDamage = (rand() % 13) + 2;
+            baseHealth = (rand() % 10) + 40;
+            baseArmor = (rand() % 10) + 25;
+            typeOfDamage = TypesOfDamage::phisical; // Non-working piece of code
+
+            return;
         }
     };
 }
--- Models/TypesOfDamage.h
+++ Models/TypesOfDamage.h
@@ -2,13 +2,19 @@
 // Created by OLEKSANDR SOKOLOV on 12/26/20.
 //
 
+#include <iostream>
+
 #ifndef CPPUNIVERSITYPROJECT_TYPESOFDAMAGE_H
 #define CPPUNIVERSITYPROJECT_TYPESOFDAMAGE_H
 
+namespace Model{
+    struct TypesOfDamage{
+        // Structure for creating types
+        // of damage for all of game classes
 
-class TypesOfDamage {
-
-};
-
+        static inline const std::string phisical = "Phisical";
+        static inline const std::string magical = "Magical";
+    } typeOfDamage_t;
+}
 
 #endif //CPPUNIVERSITYPROJECT_TYPESOFDAMAGE_H
